c633f9dbb3a78d150b7165ede83b42e63fe58022
anisali01 <anis.ali01@gmail.com>
2021-02-25 20:06:32 -0500

    app.routes for all pages done and basic formatting for secondary pages done

--- app.py
+++ app.py
@@ -45,5 +45,20 @@ def team():
     return render_template("team.html")
 
 
+@app.route("/link1")
+def link1():
+    return render_template("link1.html")
+
+
+@app.route("/link2")
+def link2():
+    return render_template("link2.html")
+
+
+@app.route("/viz")
+def viz():
+    return render_template("viz.html")
+
+
 if __name__ == "__main__":
     app.run()
